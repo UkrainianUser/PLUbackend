efc91dd0eab0cbf0066164b9d16b03b9285095f9
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2025-01-26 12:10:31 +0200

    fix name articule

--- models/PLU.js
+++ models/PLU.js
@@ -1,7 +1,7 @@
 import { Schema, model } from "mongoose";
 
 const pluSchema = new Schema({
-  article: { type: String, required: true },
+  articule: { type: String, required: true },
   name: { type: String, required: true },
   PLU: { type: String, required: true },
 });
